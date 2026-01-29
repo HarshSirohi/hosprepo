26c0c84b5a6165157b23d7543928e8f3b8e7f20b
Harsh Sirohi <harshusirohi3@gmail.com>
2026-01-29 17:44:41 +0530

    Add bcapp folder with package files

---
+++ bcapp/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app.js';
+import cloudinary from 'cloudinary';
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+dotenv.config();
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+});
